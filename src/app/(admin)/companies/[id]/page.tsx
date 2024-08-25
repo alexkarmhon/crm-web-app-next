dfac53820d445c74423bc5e53b8db2142d6a9953
alexkarmhon <89stigmat89@gmail.com>
2024-08-25 16:52:18 +0300

    fix

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,25 +1,15 @@
 'use client';
 
-import React, { useEffect } from 'react';
-
-import Header from '@/app/components/header';
-import { notFound } from 'next/navigation';
+import React from 'react';
 
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
-  useEffect(() => {
-    const id = Number.parseInt(params.id);
-    if (Number.isNaN(id)) {
-      notFound();
-    }
-  }, [params.id]);
-
   return (
-    <>
-      <Header>Company {params.id}</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
